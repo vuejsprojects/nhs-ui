ec36089441e120a13727e5af25d2173d54a7c689
pajmd <philippe@t-jmd.co.uk>
2019-02-03 09:51:59 +0000

    draft

---
+++ src/components/NhsSearch.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      Select what you want to search on
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NhsSearch",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
